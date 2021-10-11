f0ed0c3065bff456d8d373b2797fc6ba8fdae601
MiyukiKun <81800569+MiyukiKun@users.noreply.github.com>
2021-10-11 20:21:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -53,7 +53,6 @@ async def _(event):
 @bot.on(events.NewMessage(pattern="/ls"))
 async def _(event):
     p = subprocess.Popen(f'ls -lh .', stdout=subprocess.PIPE, shell=True)
-    asyncio.sleep(1)
     await event.reply(p.communicate()[0].decode("utf-8", "replace").strip())
     
 
