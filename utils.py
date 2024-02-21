7205fb4b9c585d0be8a2fac3688145638c711ef6
Hardik Majethia <81800569+MiyukiKun@users.noreply.github.com>
2024-02-22 02:03:18 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -92,3 +92,14 @@ async def download_torrent(link, event):
     await bot.send_message(event.chat_id, f"Elapsed Time: {int((end-begin)//60)} min :{int((end-begin)%60)} sec")
 
     return handle.name()
+
+def delete_files(folder):
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
