3c5ceba69aac139ec9d824218e54e2d6f4d58b1f
Hardik Majethia <81800569+MiyukiKun@users.noreply.github.com>
2024-02-23 09:45:35 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -99,7 +99,7 @@ def delete_files(folder):
         file_path = os.path.join(folder, filename)
         try:
             if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
+                os.removd(file_path)
             elif os.path.isdir(file_path):
                 shutil.rmtree(file_path)
         except Exception as e:
